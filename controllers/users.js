ce6a6d4e05d06cd758ad02891c5c52d17c40ea91
viragbalazs <viragbalazs95@gmail.com>
2018-12-06 16:39:31 +0100

    encoding

---
+++ controllers/users.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const users = express();
+const models = require('../models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+//  create
+
+users.post('/', (req, res) => {
+  models.User.findOne({ where: { username: req.body.username } }).then(user => {
+    if (user) {
+      return res.status(400).send('Van már ilyen felhasználónév, kérem adjon meg újat');
+    }
+    bcrypt.hash(req.body.password, 10).then(hash => {
+      req.body.encryptedPassword = hash;
+      models.User.create(req.body).then(user => {
+        res.json(user);
+      });
+    });
+  });
+});
+
+users.post('/login', (req, res) => {
+  models.User.findOne({
+    where: { username: req.body.username }
+  }).then(user => {
+    if (!user) {
+      return res.status(400).send('Nincs ilyen felhasználó');
+    }
+    bcrypt.compare(req.body.password, user.encryptedPassword)
+      .then(isValid => {
+        if (!isValid) {
+          return res.status(401).send('Hibás jelszó...:(');
+        }
+        const token = jwt.sign({ id: user.id }, process.env.SECRET, { expiresIn: '1h' }).toString();
+        res.json({token: token});
+      });
+  });
+});
+
+users.get('/me', (req, res) => {
+  res.json(req.user);
+});
+
+users.put('/me', (req, res) => {
+  req.user.update(req.body).then(user => {
+    res.json(user);
+  });
+});
+module.exports = users;
--- index.js
+++ index.js
@@ -2,9 +2,31 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const tasks = require('./controllers/tasks');
+const users = require('./controllers/users');
+const jwt = require('jsonwebtoken');
+const models = require('./models');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use((req, res, next) => {
+  if (req.header('x-auth')) {
+    let token = req.header('x-auth');
+    let decoded;
+    try {
+      decoded = jwt.verify(token, process.env.SECRET);
+    } catch (err) {
+      return res.status(401).send(err);
+    }
+    models.User.findById(decoded.id).then(user => {
+      req.user = user;
+      next();
+    });
+  } else {
+    next();
+  }
+});
+
 app.use('/tasks', tasks);
+app.use('/users', users);
 
 app.listen(process.env.PORT);
